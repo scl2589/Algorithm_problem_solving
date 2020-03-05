a4a5ca136a8bae6ce91a32bece3f75e110879c5d
scl2589 <cls561@nyu.edu>
2020-03-05 23:50:17 +0900

    Update 5215_햄버거 다이어트3.py

--- SWEA/5215_햄버거 다이어트/5215_햄버거 다이어트3.py
+++ SWEA/5215_햄버거 다이어트/5215_햄버거 다이어트3.py
@@ -1,28 +1,27 @@
-#시간초과..
-def f(total_calories, score):
-    global max_score
+#비트연산자 사용 (다른 사람 코드)
+tc = int(input())
+for i in range(0, tc):
+    tmp = input().split(' ')
+    burger_num = int(tmp[0])
+    max_cal = int(tmp[1])
+    max_val = 0
+    scores = []
+    cals = []
+    for j in range(0, burger_num):
+        tmp = input().split(' ')
+        scores.append(int(tmp[0]))
+        cals.append(int(tmp[1]))
 
-    for i in range(N):
-        if visited[i] == 0:
-            visited[i] = 1
-            if total_calories + table[i][1] <= L:
-                f(total_calories + table[i][1], score +table[i][0])
-            else:
-                if max_score < score:
-                    max_score = score
-                visited[i] = 0
-                return
-            visited[i] = 0
-
-
-T = int(input())
-for tc in range(1, T+1):
-    #재료의 수, 칼로리
-    N, L = map(int, input().split())
-    table = [list(map(int, input().split())) for _ in range(N)]
-    visited = [0] * N
-    max_score = 0
-    f(0, 0)
-    print('#{} {}'.format(tc, max_score))
-
-
+    for j in range(0, (1<<burger_num)):
+        sum_score = 0
+        sum_cal = 0
+        for k in range(0, burger_num):
+            if j & (1 << k):
+                sum_cal += cals[k]
+                sum_score += scores[k]
+                if sum_cal > max_cal:
+                    sum_score = 0
+                    break
+        if max_val < sum_score:
+            max_val = sum_score
+    print('#' + str(i + 1) + ' ' + str(max_val))
