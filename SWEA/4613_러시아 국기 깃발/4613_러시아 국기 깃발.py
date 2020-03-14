beb04011d16dd11cd7b2116f563f3c47ea8e0bfd
신채린 <chaelinshin96@gmail.com>
2020-03-14 23:45:03 +0900

    Update 4613 러시아 국기 깃발

--- SWEA/4613_러시아 국기 깃발/4613_러시아 국기 깃발.py
+++ SWEA/4613_러시아 국기 깃발/4613_러시아 국기 깃발.py
@@ -1,8 +1,24 @@
-#아직 미완성
-arr = [1,2,3,4,5]; N = len(arr)
-for i in range(0, N-3+1):
-    for j in range(i+1, N-2+1):
-        print(arr[:i+1], end='')
-        print(arr[i+1:j+1], end='')
-        print(arr[j+1:N], end='')
-        print()
+for tc in range(1, int(input())+1):
+    N, M = map(int, input().split()) #국기의 크기
+    arr = [input() for _ in range(N)] #오래된 국기의 색 정보
+    
+    for i in range(0, N-3+1):
+        for j in range(i+1, N-2+1):
+            cnt = 0
+            #흰색 영역: 0~i까지
+            for r in range(0, i+1):
+                for c in range(M):
+                    if arr[r][c]!='W': cnt +=1
+            #파란색: i+1~j까지
+            for r in range(i+1, j+1):
+                for c in range(M):
+                    if arr[r][c] != 'B': cnt += 1
+                        
+            #빨간색: j+1, N-1까지
+            for r in range(j+1, N):
+                for c in range(M):
+                    if arr[r][c] != 'R': cnt += 1
+                       
+            ans = min(ans, cnt)
+                
+                
