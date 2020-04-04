02d23f9341aaa932de10b55614f0892a43e5bda0
신채린 <chaelinshin96@gmail.com>
2020-04-04 23:52:52 +0900

    Update 5097 회전

--- SWEA/5097_회전/5097_회전.py
+++ SWEA/5097_회전/5097_회전.py
@@ -1,7 +1,8 @@
+from collections import deque
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int,input().split())
-    arr = list(map(int, input().split()))
-    for i in range(M):
-        arr.append(arr.pop(0))
+    arr = deque(map(int, input().split()))
+    for _ in range(M):
+        arr.append(arr.popleft())
     print('#{} {}'.format(tc, arr[0]))
