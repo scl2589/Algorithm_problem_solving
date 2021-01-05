8a982d3b900eb28107764a2b56ed0f80cd31110d
Chae Lin Shin <chaelinshin96@gmail.com>
2021-01-05 18:43:55 +0900

    Add 15659 N과 M

--- Baekjoon/15649_N과 M/15649_N과 M.py
+++ Baekjoon/15649_N과 M/15649_N과 M.py
@@ -1,6 +1,29 @@
-from itertools import permutations
+# 방법 1
+
+# from itertools import permutations
+# N, M = map(int, input().split())
+# arr = [i for i in range(1, N + 1)]
+# arr2 = list(permutations(arr, M))
+# for i in arr2:
+#     print(*i)
+
+
+# 방법 2
 N, M = map(int, input().split())
-arr = [i for i in range(1, N + 1)]
-arr2 = list(permutations(arr, M))
-for i in arr2:
-    print(*i)
+arr = [i for i in range(1, N+1)]
+visited = [False] * N 
+
+def backtracking(n, k, ans):
+    global visited
+    if n == k:
+        print(ans)
+        return 
+    for i in range(len(visited)):
+        if not visited[i]:
+            visited[i] = True
+            backtracking(n+1, k, ans.append(arr[i]))
+            visited[i] = False 
+            backtracking(n, k, ans)
+
+
+backtracking(0, M, [])
