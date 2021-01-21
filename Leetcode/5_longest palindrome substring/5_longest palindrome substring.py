3e6d5e6bc7355ea66602269d7c6acc5026601cb3
scl2589 <chaelinshin96@gmail.com>
2021-01-22 00:55:46 +0900

    Update longest palindrome substring

--- Leetcode/5_longest palindrome substring/5_longest palindrome substring.py
+++ Leetcode/5_longest palindrome substring/5_longest palindrome substring.py
@@ -16,7 +16,7 @@ class Solution:
             answers.sort(key = lambda x : len(x), reverse=True)
             return answers[0]
 
-# 올바른 풀이
+# 올바른 풀이 (이 방식이 하단의 투 포인터 풀이보다 빠르다)
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         if len(s) <= 1:
@@ -30,3 +30,26 @@ class Solution:
                 i, l = j-l-1, l+2
                 # print(s[i: i+l])
         return s[i: i+l]
+
+# 투포인터 방식으로 풀이
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # 팰린드롬 판별 및 투 포인터 확장
+        def expand(left: int, right: int) -> str:
+            while left >= 0 and right <= len(s) and s[left] == s[right-1]:
+                left -= 1
+                right += 1
+            return s[left+1:right-1]
+    
+        # 해당 사항이 없을 경우엔 빠르게 리턴
+        if len(s) < 2 or s == s[::-1]:
+            return s
+    
+        result = ''
+        # 슬라이딩 윈도우 우측으로 이동
+        for i in range(len(s)-1):
+            result = max(result,
+                        expand(i, i+1), 
+                        expand(i, i+2),
+                        key = len)
+        return result
