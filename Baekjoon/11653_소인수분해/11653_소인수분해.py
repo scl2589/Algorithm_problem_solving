16ec4821002afe0edc281f8324d452dea9deceaf
scl2589 <chaelinshin96@gmail.com>
2021-01-25 23:08:51 +0900

    Update 11653_소인수분해 - 1 제외

--- Baekjoon/11653_소인수분해/11653_소인수분해.py
+++ Baekjoon/11653_소인수분해/11653_소인수분해.py
@@ -1,10 +1,13 @@
 N = int(input())
-while True:
-    for i in range(2, N):
-        if N % i == 0:
-            print(i)
-            N //= i
-            break 
-    else:
-        print(N)
-        break
+if N == 1:
+    pass
+else:
+    while True:
+        for i in range(2, N):
+            if N % i == 0:
+                print(i)
+                N //= i
+                break 
+        else:
+            print(N)
+            break
