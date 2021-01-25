d35c937bec3614356149e70d653e383b9102dcff
scl2589 <chaelinshin96@gmail.com>
2021-01-26 00:00:52 +0900

    Add 1874 스택 수열

--- Baekjoon/1874_스택 수열/1874_스택 수열.py
+++ Baekjoon/1874_스택 수열/1874_스택 수열.py
@@ -1,4 +1,23 @@
+#다시 풀기 
+
 N = int(input())
-stack = []
-for i in range(1, N+1):
-    
+s = []
+op = []
+count = 1
+temp = True
+for i in range(N):
+    num = int(input())
+    while count <= num:
+        s.append(count)
+        op.append('+')
+        count += 1
+    if s[-1] == num:
+        s.pop()
+        op.append('-')
+    else:
+        temp = False 
+if temp == False:
+    print("NO")
+else:
+    for i in op:
+        print(i)
