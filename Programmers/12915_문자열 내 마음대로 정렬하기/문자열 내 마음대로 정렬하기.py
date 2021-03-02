4f6a6689ebc1731a2b99bc6dfe0b0245fbf1216b
scl2589 <chaelinshin96@gmail.com>
2021-03-02 22:15:40 +0900

    Add 문자열 내 마음대로 정렬하기

---
+++ Programmers/12915_문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -0,0 +1,19 @@
+def solution(strings, n):
+    answers = sorted(sorted(strings), key = lambda x : (x[n]))
+    return answers
+
+
+'''
+테스트 1 〉	통과 (0.01ms, 10.2MB)
+테스트 2 〉	통과 (0.01ms, 10.3MB)
+테스트 3 〉	통과 (0.02ms, 10.2MB)
+테스트 4 〉	통과 (0.02ms, 10.1MB)
+테스트 5 〉	통과 (0.01ms, 10.2MB)
+테스트 6 〉	통과 (0.02ms, 10.2MB)
+테스트 7 〉	통과 (0.01ms, 10.2MB)
+테스트 8 〉	통과 (0.01ms, 10.1MB)
+테스트 9 〉	통과 (0.01ms, 10.1MB)
+테스트 10 〉	통과 (0.02ms, 10.2MB)
+테스트 11 〉	통과 (0.01ms, 10.2MB)
+테스트 12 〉	통과 (0.02ms, 10.1MB)
+'''
