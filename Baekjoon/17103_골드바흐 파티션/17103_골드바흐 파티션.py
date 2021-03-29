f6a67cf3af25259fcc294bd548f70c1bc99a5bc2
Chae Lin Shin <cls561@nyu.edu>
2021-03-29 15:38:37 +0900

    Update 17103 골드바흐 파티션 - 더 빠른 코드

--- Baekjoon/17103_골드바흐 파티션/17103_골드바흐 파티션.py
+++ Baekjoon/17103_골드바흐 파티션/17103_골드바흐 파티션.py
@@ -16,3 +16,22 @@ for num in nums:
         if primes[i] and primes[num-i]:
             count += 1
     print(count)
+
+# 더 빠른 코드
+import sys
+prime = [True] * 1000001
+prime[0] = prime[1] = False
+prime_list = []
+
+for i in range(2, 500001):
+    if not prime[i]: continue
+    for j in range(i**2, 1000001, i): prime[j] = False
+    prime_list.append(i)
+
+for i in range(int(sys.stdin.readline())):
+    n = int(sys.stdin.readline())
+    tmp = 0
+    for j in prime_list:
+        if j > n//2: break
+        if prime[n-j]: tmp +=1
+    print(tmp)
