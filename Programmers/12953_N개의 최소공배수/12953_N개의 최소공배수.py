2a0fd30b7df10e016b8cf724aeeaa99a6a6abf53
Chae Lin Shin <cls561@nyu.edu>
2021-04-01 22:15:30 +0900

    Update 12953 N개의 최소공배수 - correct answer

--- Programmers/12953_N개의 최소공배수/12953_N개의 최소공배수.py
+++ Programmers/12953_N개의 최소공배수/12953_N개의 최소공배수.py
@@ -1,4 +1,18 @@
+# 정답 코드 
 import math
+def solution(arr):
+    answer = 1
+    if len(arr) == 1:
+        return arr[0]
+    while len(arr) >= 2:
+        a = arr.pop()
+        b = arr.pop() 
+        temp_gcd = math.gcd(a, b) 
+        temp_lcd = (a * b) // temp_gcd
+        arr.append(temp_lcd)
+    return arr[0]
+
+# 8개/10개 (틀린 코드)
 def solution(arr):
     answer = 1
     while True:
