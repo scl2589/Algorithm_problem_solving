7131c25c892b9c621079f877f3a5209b51105b17
Chae Lin Shin <chaelinshin96@gmail.com>
2021-04-07 20:58:34 +0900

    Add Leetcode 20 valid parentheses - better solution

---
+++ Leetcode/20_valid parentheses/20_valid parentheses2.java
@@ -0,0 +1,27 @@
+//better solution
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '[' || c == '{'){
+                stack.push(c);
+            }else {
+                
+                if(stack.isEmpty()){
+                    return false;
+                }
+                
+                if(c == ')' && stack.peek() != '('){
+                    return false;
+                }else if(c == ']' && stack.peek() != '['){
+                    return false;
+                }else if(c == '}' && stack.peek() != '{'){
+                    return false;
+                }
+                
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
